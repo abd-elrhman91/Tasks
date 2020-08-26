77f7147e42b7b3e188abd42f3c3d7ca95a0b538e
abd-elrhman91 <69834684+abd-elrhman91@users.noreply.github.com>
2020-08-26 22:29:11 +0200

    Add files via upload

---
+++ count2.py
@@ -0,0 +1,39 @@
+import string
+fname=input("please entre the file's name : ")
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened:')
+    exit()
+
+counts = dict()
+for line in fhand:
+    line = line.translate(line.maketrans('', '', string.punctuation))
+    words = line.split()
+    for word in words:
+        if word not in counts:
+            counts[word] = 1
+        else:
+            counts[word] += 1
+            
+
+lst=list()
+
+
+for key,val in counts.items():
+	lst.append(  (val , key)  )
+	
+lst.sort(reverse=True)
+del lst[8]
+for key  , val in lst[:10]:
+	print(  val  , key )
+
+
+	
+	
+	
+	
+	
+	
+	
+	
